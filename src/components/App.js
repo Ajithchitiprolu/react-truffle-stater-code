271f1f4a913f9b1e43547dcb2e52d37a47122f57
Ajithchitiprolu <chitiprolu.ajith@gmail.com>
2021-09-18 12:11:17 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    
+    <div>
+      <h1 className="text-red-500 text-4xl text-center bg-green-300">
+        Code Here!
+      </h1>
+    </div>
+    
+    
+  );
+}
+
+export default App;
